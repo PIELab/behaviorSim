3c1c96014151e84528b76e4a88e6152023b906d6
Tylar <murray.tylar@gmail.com>
2013-05-28 01:13:49 -0400

    working on agents class, added more comments and explanation in inputs, added more to sampleScript

--- inputs/inputs.py
+++ inputs/inputs.py
@@ -1,8 +1,12 @@
-# the inputs class initializes all possible inputs for the model, but calls external methods for loading the inputs. 
+# the inputs class initializes inputs for the model and calls external methods for loading the inputs based on the inputType (to match the model) and the specific input being used (an instance of the inputType). 
+# to organize this structure, inputTypes (or models) are subpackages of the inputs package, and instances of the model are modules of the inputType package. Thus, the general structure is something like inputs.model.instance to reference the desired module. This is particularly troublesome when a specific input may be used by more than one model or when combining two models. To simplify references to input, all input is referenced through this inputs class directly. That way this is the only file which needs to be changed when switching inputs.
 
 # set input file locations here:
 from .CSEL import example as CSEL
-# add new ones like this:
+#alternative model instance:
+#from .CSEL import otherExample as CSEL
+
+# add new models like this:
 # from .InputType import typeInstance as InputType
 
 class inputs:
--- sampleScript.py
+++ sampleScript.py
@@ -1,5 +1,14 @@
+# to load the 'standard' input and agent:
 import inputs.inputs as inputs
-
+import agents.agents as agents
 sampleInput = inputs.inputs()
+sampleAgent = agents.agents()
+# these agents can be modified/added to following editing instructions in ./inputs/inputs.py and ./agents/agents.py. 
+
+print sampleInput.xi(5)		# print exogeneous flow vars @ t=5
 
 
+# alternatively, specific instances of input/agents can be loaded directly like:
+import inputs.PECS.example
+
+print inputs.PECS.Example.belief(5)	# print belief @ t=5
