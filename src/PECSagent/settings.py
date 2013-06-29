3832674dfb27aba7cefc88b27f8d4cd74bd5fb0c
7yl4r <murray.tylar@gmail.com>
2013-06-29 11:47:07 -0400

    made CSEL plot look more like it should, increased subSteps in settings to get better results

--- src/PECSagent/settings.py
+++ src/PECSagent/settings.py
@@ -6,7 +6,7 @@ import logging
 class settings:
 	deltaTime    = timedelta(days=1)	# sim-world time between steps
 	simStartTime = datetime(2013, 5, 30, 11, 17, 0, 0) # sim-time of the beginning of the simulation NOTE: all agents are assumed to be in the same time zone so no tzinfo is included here.
-	subSteps     = 2	# used for integrations; when a continuous function is specified, how many subSteps per deltaTime are computed?
+	subSteps     = 100	# used for integrations; when a continuous function is specified, how many subSteps per deltaTime are computed? MUST be even?!?
 
 	# this sets up a log config if one hasn't been set yet. If one is set previously, this one is ignored.
 	logging.basicConfig(filename='src/__logs/defaultLogFile.log',\
